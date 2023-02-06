1664b5787ee5d9fa4689529d0554624d218e723e
Ahmad Rana <rana456ahmad789@gmail.com>
2023-02-07 01:12:45 +0500

    new functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
- console.log('hello world');
+const todos = [
+	{
+		id: 1,
+		text: 'do your home work quickly',
+		isDone: true,
+	},
+	{
+		id: 2,
+		text: 'have done the task',
+		isDone: true,
+	},
+	{
+		id: 3,
+		text: 'make sure to check it',
+		isDone: true,
+	},
+	{
+		id: 1,
+		text: 'i am comming to check it ',
+		isDone: false,
+	},
+];
+const todoJSON = JSON.stringify(todos);
+console.log(todoJSON);
