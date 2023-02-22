c200978f2bd66734daaa4d88bb4b9f1cee97a7fd
Ahmad Rana <rana456ahmad789@gmail.com>
2023-02-22 23:17:27 +0500

    counter run app

---
+++ new project.js
@@ -0,0 +1,10 @@
+// let  bonusPoint = 50
+// bonusPoint = bonusPoint + 50
+
+// bonusPoint = bonusPoint - 75
+// bonusPoint = bonusPoint + 50
+// console.log(bonusPoint)
+
+function increment() {
+	console.log('button was clicked');
+}
